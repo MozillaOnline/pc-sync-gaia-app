bcb40481b58cd934e95d2f518ed1c9fcfe46eb3a
dxue <dxue@mozilla.com>
2012-12-25 15:14:07 +0800

    add pcsyncclient code

---
+++ pcsyncclient/js/action-contact.js
@@ -0,0 +1,133 @@
+
+var Action_contact = {
+
+	request: function (data){
+		dump('xds4' + JSON.stringify(data));
+		switch (data.command) {
+			case "getAllContacts":{
+				var options = {
+					sortBy: 'familyName',
+					sortOrder: 'ascending'
+				};
+				var request = window.navigator.mozContacts.find(options);
+				request.onsuccess = function findCallback() {
+					Action_contact.success(data, request.result);
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			case "getContact":{
+				var options = {
+					filterBy: ['id'],
+					filterOp: 'equals',
+					filterValue: data.data
+				};
+				var request = window.navigator.mozContacts.find(options);
+				request.onsuccess = function findCallback(e) {
+					var result = e.target.result[0];
+					Action_contact.success(data, result);
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			case "removeContact":{
+				var options = {
+					filterBy: ['id'],
+					filterOp: 'equals',
+					filterValue: data.data
+				};
+				var request = window.navigator.mozContacts.find(options);
+				request.onsuccess = function findCallback(e) {
+					var result = e.target.result[0];
+					var request2 = window.navigator.mozContacts.remove(result);
+					request2.onsuccess = function findCallback(e) {
+						Action_contact.success(data, data.data);
+					}
+					request2.onerror = function findCallback() {
+						Action_contact.error(data, result);
+					};
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			case "clearContacts":{
+				var request = window.navigator.mozContacts.clear();
+				request.onsuccess = function findCallback() {
+					Action_contact.success(data, result);
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			case "addContact":{
+				var contact = new mozContact();
+				contact.init(data.data[0]);
+				var request = navigator.mozContacts.save(contact);
+				request.onsuccess = function findCallback() {
+					Action_contact.success(data, contact);
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			case "updateContact":{
+				var request = navigator.mozContacts.save(data.data[0]);
+				request.onsuccess = function findCallback() {
+					Action_contact.success(data, data.data[0]);
+				};
+				request.onerror = function findCallback() {
+					Action_contact.error(data, request.result);
+				};
+				break;
+			}
+			default:
+				alert("contactrequest Received msg from mgmt: " + data);
+				break;
+		}
+	},
+	
+	response: function (data){
+		switch (data.command) {
+			default:
+				alert("contactrequest Received msg from mgmt: " + data);
+				break;
+		}
+	},
+	
+	success: function (data, result){
+		var contactdata = {
+			action: 'response',
+			id: data.id,
+			command: data.command,
+			status: 200,
+			data: result
+		};
+		dump(JSON.stringify(contactdata));
+		Connection_internet.ws.send(JSON.stringify(contactdata));
+		/*
+		 * * alert(request.result[0].photo[0].size);
+		 * */
+	},
+	
+	error: function (data){
+		var contactdata = {
+			action: 'response',
+			id: data.id,
+			command: data.command,
+			status: 201,
+			data: result
+		};
+		dump(JSON.stringify(contactdata));
+		Connection_internet.ws.send(JSON.stringify(contactdata));
+	}
+	
+};
+
---
+++ pcsyncclient/js/connection-internet.js
@@ -0,0 +1,97 @@
+
+var Connection_internet = {
+	ws: null,
+	retcallback: null,
+	
+	connectToServer: function (url, callback){
+		this.retcallback = callback;
+		window.WebSocket = window.WebSocket || window.MozWebSocket;
+		this.ws = new WebSocket(url);
+		this.ws.onopen = function() {
+			var navigator = window.navigator;
+			var registerdata = null;
+			if ('mozWifiManager' in navigator){
+				registerdata = navigator.mozWifiManager.macAddress;
+			}
+			else{
+				registerdata = 'tempdata';
+			}
+			var register = {
+				action: 'request',
+				id: 0,
+				target: 'init',
+				command: 'register',
+				data: registerdata
+				};
+			alert(registerdata);
+			Connection_internet.ws.send(JSON.stringify(register));
+			
+		};
+		this.ws.onmessage = function(event) {
+			Connection_internet.handleMessage(event.data);
+		};
+		this.ws.onerror = function(event) {
+			Connection_internet.retcallback('connect-failed');
+		};
+	},
+	
+	disconnectToServer: function (){
+		if (this.ws) {
+			this.ws.close();
+			this.ws = null;
+		}
+	},
+	
+	handleMessage: function (data){
+		var message = null;
+		try {
+			message = JSON.parse(data);
+		} 
+		catch (e) {
+			alert("Parse error, received msg from mgmt: " + data);
+			return;
+		}
+		dump(JSON.stringify(message));
+		switch (message.action) {
+			case "request":
+				this.requestmessage(message);
+				break;
+			case "response":
+				this.responsemessage(message);
+				break;
+			default:
+				alert("handleMessage Received msg from mgmt: " + data);
+				break;
+		}
+	},
+	
+	requestmessage: function (data){
+		switch (data.target) {
+			case "contact":
+				Action_contact.request(data);
+				break;
+			default:
+				alert("requestmessage Received msg from mgmt: " + data);
+				break;
+		}
+	},
+	
+	responsemessage: function (data){
+		switch (data.command) {
+			case "register":
+				this.registerresponse(data);
+				break;
+			default:
+				alert("responsemessage Received msg from mgmt: " + data);
+				break;
+		}
+	},
+	
+	registerresponse: function (data){
+		if(data.status == 200)
+			this.retcallback(0);
+		else
+			this.retcallback('not-authorized');
+	}
+};
+
