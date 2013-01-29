29f15dcb004856ac42629be47cad60a970028905
pzhang <pzhang@mozilla.com>
2013-01-29 16:04:39 +0800

    Handle multiple connections

--- pcsyncclient/js/background.js
+++ pcsyncclient/js/background.js
@@ -2,11 +2,11 @@
 var usbm = new MozUSBManager();
 
 var descriptor = {
-	deviceClass: n,
-	deviceSubClass: n,
-	deviceProtocol: n,
-	deviceVendor: n,
-	deviceProduct: n
+  deviceClass: n,
+  deviceSubClass: n,
+  deviceProtocol: n,
+  deviceVendor: n,
+  deviceProduct: n
 };
 
 usbm.addEventListener("attachdevice", function () {
@@ -25,34 +25,37 @@ usbm.claimDevice(descriptor);
 */
 
 var Connection_usb = {
-	PORT: 10010,
-	BACKLOG: -1,
-	options: { binaryType: 'string' },
-	server: null,
-	acceptsock: null,
-	
-	createSocketserver: function (){
-		dump('pcsync background.js line36');
-		this.server = window.navigator.mozTCPSocket.listen(this.PORT, this.options, this.BACKLOG);
-		if(this.server){
-			this.server.onaccept = function(socket) {
-				dump('pcsync background.js line40 :' + socket);
-				Connection_usb.acceptsock = socket;
-				Client_message.init(Connection_usb.acceptsock);
-				Connection_usb.acceptsock.ondata = function(event) {
-					Client_message.handleMessage(event.data);
-				};
-				Connection_usb.acceptsock.onclose = function(event) {
-					//Connection_usb.createSocketserver(Connection_usb.retcallback);
-					dump('pcsync background.js line48');
-				};
-			};
-		}
-		else{
-			dump('pcsync background.js line53');
-		}
-		
-	}
+  PORT: 10010,
+  BACKLOG: -1,
+  options: { binaryType: 'string' },
+  server: null,
+  acceptsock: null,
+
+  createSocketserver: function (){
+    dump('pcsync background.js line36');
+    this.server = window.navigator.mozTCPSocket.listen(this.PORT, this.options, this.BACKLOG);
+    if(this.server){
+      this.server.onaccept = function(socket) {
+        dump('pcsync background.js line40 :' + socket);
+        Connection_usb.acceptsock = socket;
+
+        var messageHandler = new Client_message(socket);
+
+        socket.ondata = function(event) {
+          this.handleMessage(event.data);
+        }.bind(messageHandler);
+
+        socket.onclose = function(event) {
+          //Connection_usb.createSocketserver(Connection_usb.retcallback);
+          dump('pcsync background.js line49');
+        }.bind(messageHandler);
+      };
+    }
+    else{
+      dump('pcsync background.js line53');
+    }
+
+  }
 };
 
 Connection_usb.createSocketserver();
--- pcsyncclient/js/client-message.js
+++ pcsyncclient/js/client-message.js
@@ -1,62 +1,59 @@
+function Client_message(socket) {
+  dump('pcsync client-message.js line2 :' + socket);
+  this.acceptsock = socket;
+}
 
-var Client_message = {
-	acceptsock: null,
-	
-	init: function (sockethandle){
-		dump('pcsync client-message.js line6 :' + sockethandle);
-		this.acceptsock = sockethandle;
-	},
-	
-	handleMessage: function (data){
-		var message = null;
-		dump('pcsync client-message.js line11 :' + JSON.stringify(data));
-		try {
-			message = JSON.parse(data);
-		} 
-		catch (e) {
-			dump('pcsync client-message.js line16 :' + data);
-			return;
-		}
-		switch (message.action) {
-			case "request":
-				this.requestmessage(message);
-				break;
-			case "response":
-				this.responsemessage(message);
-				break;
-			default:
-				dump('pcsync client-message.js line27 :' + data);
-				break;
-		}
-	},
-	
-	requestmessage: function (data){
-		switch (data.target) {
-			case "contact":
-				dump('pcsync client-message.js line35 :' + this.acceptsock);
-				Action_contact.init(this.acceptsock);
-				Action_contact.request(data);
-				break;
-			case "sms":
-				Action_sms.init(this.acceptsock);
-				Action_sms.request(data);
-				break;
-			case "device":
-				Action_device.init(this.acceptsock);
-				Action_device.request(data);
-				break;
-			default:
-				dump('pcsync client-message.js line43 :' + data);
-				break;
-		}
-	},
-	
-	responsemessage: function (data){
-		switch (data.target) {
-			default:
-				dump('pcsync client-message.js line51 :' + data);
-				break;
-		}
-	}
+Client_message.prototype = {
+  handleMessage: function (data){
+    var message = null;
+    dump('pcsync client-message.js line11 :' + JSON.stringify(data));
+    try {
+      message = JSON.parse(data);
+    }
+    catch (e) {
+      dump('pcsync client-message.js line16 :' + data);
+      return;
+    }
+    switch (message.action) {
+      case "request":
+        this.requestmessage(message);
+        break;
+      case "response":
+        this.responsemessage(message);
+        break;
+      default:
+        dump('pcsync client-message.js line27 :' + data);
+        break;
+    }
+  },
+
+  requestmessage: function (data){
+    switch (data.target) {
+      case "contact":
+        dump('pcsync client-message.js line35 :' + this.acceptsock);
+        Action_contact.init(this.acceptsock);
+        Action_contact.request(data);
+        break;
+      case "sms":
+        Action_sms.init(this.acceptsock);
+        Action_sms.request(data);
+        break;
+      case "device":
+        Action_device.init(this.acceptsock);
+        Action_device.request(data);
+        break;
+      default:
+        dump('pcsync client-message.js line43 :' + data);
+        break;
+    }
+  },
+
+  responsemessage: function (data){
+    switch (data.target) {
+      default:
+        dump('pcsync client-message.js line51 :' + data);
+        break;
+    }
+  }
 };
 
