bf7410a5a842f23ee59e543cc9fe1a5af878289f
dxue <dxue@mozilla.com>
2013-03-14 15:39:13 +0800

    change socket server

--- pcsyncclient/js/background.js
+++ pcsyncclient/js/background.js
@@ -1,48 +1,47 @@
+/*
+var usbm = new MozUSBManager();
+
+var descriptor = {
+  deviceClass: n,
+  deviceSubClass: n,
+  deviceProtocol: n,
+  deviceVendor: n,
+  deviceProduct: n
+};
+
+usbm.addEventListener("attachdevice", function () {
+// ...
+debug("pcsync USB connected");
+
+});
+
+usbm.addEventListener("detachdevice", function () {
+// ...
+debug("pcsync USB disconnected");
+
+});
+
+usbm.claimDevice(descriptor);
+*/
 
 var Connection_usb = {
   PORT: 10010,
   BACKLOG: -1,
-  options: { binaryType: 'arraybuffer' },
+  options: {
+    binaryType: 'arraybuffer'
+  },
   server: null,
-  acceptsock: null,
 
-  createSocketserver: function (){
-    debug('pcsync background.js line36');
+  createSocketserver: function() {
+    debug('pcsync background.js line34');
     this.server = window.navigator.mozTCPSocket.listen(this.PORT, this.options, this.BACKLOG);
-    if(this.server){
+    if (this.server) {
       this.server.onaccept = function(socket) {
-        debug('pcsync background.js line40 :' + socket);
-        Connection_usb.acceptsock = socket;
-
-        var messageHandler = new Client_message(socket);
-
-        socket.ondata = function(event) {
-         // while(event.data.length>0){
-         //   debug("receive data length:" + event.data.length);
-         // }
-          //debug("=== ondata complete");
-          //socket.send('=====send complete=====');
-          //debug("Receive data: " + event.data);
-          this.handleMessage(event.data);
-          debug("Handle data!!!");
-        }.bind(messageHandler);
-
-        socket.onerror = function onerror(event) {
-          debug('error occurs');
-        };
-
-        socket.ondrain = function ondrain(event) {
-          debug('on drain');
-        };
-
-        socket.onclose = function(event) {
-          //Connection_usb.createSocketserver(Connection_usb.retcallback);
-          debug('pcsync background.js line49');
-        }.bind(messageHandler);
+        debug('pcsync background.js line38 :' + socket);
+        new ConnectionHelper(socket);
       };
-    }
-    else{
-      debug('pcsync background.js line53');
+    } else {
+      debug('pcsync background.js line43');
     }
 
   }
