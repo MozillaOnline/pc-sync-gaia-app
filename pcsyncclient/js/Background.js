cfdde5ebe5f6dea9f268eb3201d2bb72ddc5d4ae
dxue <dxue@dxue-mozilla.(none)>
2013-08-14 17:33:08 +0800

    add wifi support

--- pcsyncclient/js/Background.js
+++ pcsyncclient/js/Background.js
@@ -18,22 +18,44 @@ var backgroundService = {
       var tcpServer = window.navigator.mozTCPSocket.listen(this.PORT, this.OPTIONS, this.BACKLOG);
       if (tcpServer) {
         tcpServer.onconnect = function(event) {
-          var deviceStatus = document.getElementById('menuItem-icc');
+          var deviceStatus = document.getElementById('menuItem-device-unconnected');
           if(deviceStatus){
-            deviceStatus.textContent = "Connected";
+            deviceStatus.textContent = navigator.mozL10n.get('device-connected');
           }
-          console.log('Background.js listen onconnect ' + event);
           new TCPSocketWrapper({
             socket: event,
             onmessage: handleMessage,
             onclose: function (){
-              var deviceStatus = document.getElementById('menuItem-icc');
+              var deviceStatus = document.getElementById('menuItem-device-unconnected');
               if(deviceStatus){
-                deviceStatus.textContent = "Unconnect";
+                deviceStatus.textContent = navigator.mozL10n.get('device-unconnected');
               }
             }
           });
         };
+        var wifiConnectCode = document.getElementById('menuItem-wifi-connect-number');
+        if(wifiConnectCode){
+          if ('mozWifiManager' in navigator) {
+            var gWifiManager = navigator.mozWifiManager;
+            var updateNetInfo = function() {
+              console.log('Background.js updateNetInfo');
+              var info = gWifiManager.connectionInformation;
+              if(info && info.ipAddress) {
+                var ip = info.ipAddress.split('.');
+                var dataArray = new ArrayBuffer(4);
+                var int8Array = new Uint8Array(dataArray);
+                var int32Array = new Uint32Array(dataArray);
+                int8Array[0] = ip[0];
+                int8Array[1] = ip[1];
+                int8Array[2] = ip[2];
+                int8Array[3] = ip[3];
+                wifiConnectCode.textContent = int32Array[0];
+              }
+            };
+            gWifiManager.connectionInfoUpdate = updateNetInfo;
+            updateNetInfo();
+          }
+        }
       } else {
         console.log('Background.js listen failed');
       }
@@ -43,7 +65,16 @@ var backgroundService = {
   }
 };
 
-backgroundService.createSocketServer();
+window.addEventListener('load', function startup(evt) {
+  backgroundService.createSocketServer();
+});
+
+window.addEventListener('localized', function onlocalized() {
+  // Set the 'lang' and 'dir' attributes to <html> when the page is translated
+  document.documentElement.lang = navigator.mozL10n.language.code;
+  document.documentElement.dir = navigator.mozL10n.language.direction;
+});
+
 /*
 document.addEventListener('mozvisibilitychange', function() {
   console.log('Background.js document.mozHidden: ' + document.mozHidden);
